abea867bcd79f73c1ab51b2f27555ac776d0f335
Michael Korens <mkore760@gmail.com>
2018-07-19 16:27:10 -0700

    Added new functionality

--- scanner.py
+++ scanner.py
@@ -1,16 +1,32 @@
-# Initial file
 
 from scapy.all import *
 
-packets = rdpcap('Sketchy Stuff.pcap')
+PACKETS = rdpcap('Sketchy Stuff.pcap')
 
-for packet in packets:
-	if packet[0].getlayer(IP):
-		ip_src = packet[0].getlayer(IP).src
-		ip_dst = packet[0].getlayer(IP).dst
-		if packet.haslayer(DNS) and packet.getlayer(DNS).qr == 0:
-			ip_dns = packet[0].getlayer(DNS).qd.qname
-			print("IP: " + str(ip_src) + " -> " + str(ip_dst) + " : (" + str(ip_dns))
-		# print("IP: " + str(ip_src) + " -> " + str(ip_dst))
-	#else:
-		# print("No IP found")
+enableWebsites = True
+WEBSITES = [".com", ".org", ".net", ".edu", ".co", ".ai", ".xyz", ".training", ".io"]
+
+
+def analysis(packets):
+	for pkt in packets:
+		try:
+			ip_src = str(pkt.getlayer(IP).src)
+			ip_dst = str(pkt.getlayer(IP).dst)
+			#ip_addr = str(pkt.getlayer(IP).addr)
+			if pkt.haslayer(DNS) and pkt.getlayer(DNS).qr == 0:
+
+				unsanitized_dns = str(pkt.getlayer(DNS).qd.qname)
+				ip_dns = unsanitized_dns[2:-2]
+
+				if enableWebsites:
+					for counter, debug in enumerate(WEBSITES):
+						if ip_dns[-4:] == WEBSITES[counter]:
+							print(ip_dns + " IP: " + ip_src + " Connected to " + ip_dst)
+
+				else:
+					print(ip_dns + " (IP: " + ip_src + ") Connected to " + ip_dst)
+		except AttributeError:
+			continue
+
+
+analysis(PACKETS)
